5e143a124600acd46ad7df616dee4ef8b03de663
tonas <thomas11ventura@gmail.com>
2024-03-13 20:39:05 -0300

    fixed som exercises

--- Lista4/list04ex03/src/Main.java
+++ Lista4/list04ex03/src/Main.java
@@ -5,13 +5,20 @@ public class Main {
         Scanner read = new Scanner(System.in);
 
         int [] lista = {1,2,3,4,5,6,7,8,9,10};
+        boolean j = false;
 
         System.out.println("Digite um número");
         int n1 = read.nextInt();
 
-        if  (n1 >= 0 && n1 <= 9)
-            System.out.println("Valor na posição " + n1 + ": " + lista[n1]);
-        else
+        for (int i=0; i<lista.length; i++) {
+            if (lista[i] == n1) {
+                System.out.println("Valor " + n1 + " encontrado no endereço " + i);
+                j = true;
+                break;
+            }
+        }
+
+        if (!j)
             System.out.println("Valor não encontrado");
     }
 }
